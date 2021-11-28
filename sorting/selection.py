cc5d48f1e89ed9a3eef8a525aefac752202c9d81
balakumar <bala93kumar@gmail.com>
2021-11-28 23:02:24 +0530

    selection sort

--- sorting/selection.py
+++ sorting/selection.py
@@ -1,3 +1,27 @@
 
 
 
+
+
+def selectionSort(arr,n):
+
+
+    for i in range(0, n):
+        min_idx =  i
+        for j in range(i+1,n):
+            if arr[j] < arr[min_idx]:
+                min_idx  = j
+        arr[i], arr[min_idx] = arr[min_idx],   arr[i]
+
+
+
+
+
+
+if __name__ == "__main__":
+
+    arr = [5,6,10,9,2]
+
+    selectionSort(arr,5)
+
+    print(arr)
